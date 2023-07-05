eed50c074b783c19266ba22f21dbb17b1f58ea7e
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-07-05 11:58:50 -0500

    non functioning url replacement

--- src/components/SearchResult/SearchResult.jsx
+++ src/components/SearchResult/SearchResult.jsx
@@ -11,62 +11,69 @@ const SearchResult = () => {
     const addToCatalog = (game) => {
       dispatch({type:'ADD_CATALOG', payload: game})
     }
+   
 
-return (
-  <>
-  <br /><br />
-  <div>
-    <div className="nes-table-responsive">
-      <table className="nes-table is-bordered is-centered">
-        <thead>
-          <tr>
-            <th>Picture</th>
-            <th>Game</th>
-            <th>Add to Wishlist</th>
-            <th>Add to Catalog</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gameListReducer.length !== 0 ? (
-            gameListReducer.map((game, i) => (
-              <tr key={i}>
-                <td>
-                  {game.cover && game.cover.url && (
-                    <img src={game.cover.url} alt="Cover" />
-                  )}
-                </td>
-                <td>{game.name}</td>
-                <td className="button-container">
-                  <button
-                    onClick={() => addToWishlist(game)}
-                    type="button"
-                    className="nes-btn is-primary"
-                  >
-                    Add to Wishlist
-                  </button>
-                </td>
-                <td className="button-container">
-                  <button
-                    onClick={() => addToCatalog(game)}
-                    type="button"
-                    className="nes-btn is-primary"
-                  >
-                    Add to Catalog
-                  </button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="4">No games available</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
-  </div>
-  </>
-);
+    return (
+      <>
+        <br /><br />
+        <div>
+          <div className="nes-table-responsive">
+            <table className="nes-table is-bordered is-centered">
+              <thead>
+                <tr>
+                  <th>Picture</th>
+                  <th>Game</th>
+                  <th>Add to Wishlist</th>
+                  <th>Add to Catalog</th>
+                </tr>
+              </thead>
+              <tbody>
+                {gameListReducer.length !== 0 ? (
+                  gameListReducer.map((game, i) => {
+                    const image = game.cover.url;
+                    const finalUrl = image && image.replace('/t_thumb/', '/t_cover_big');
+    
+                    return (
+                      <tr key={i}>
+                        <td>
+                          {finalUrl && (
+                            <img src={finalUrl} alt="Cover" />
+                          )}
+                        </td>
+                        <td>{game.name}</td>
+                        <td className="button-container">
+                          <button
+                            onClick={() => addToWishlist(game)}
+                            type="button"
+                            className="nes-btn is-primary"
+                          >
+                            Add to Wishlist
+                          </button>
+                        </td>
+                        <td className="button-container">
+                          <button
+                            onClick={() => addToCatalog(game)}
+                            type="button"
+                            className="nes-btn is-primary"
+                          >
+                            Add to Catalog
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan="4">No games available</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
+    );
+    
 
 
       }
