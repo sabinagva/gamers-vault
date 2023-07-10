b0bc74fe9b859ebcb1991a6fe3cb272b719918aa
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-07-10 13:44:17 -0500

    added files to gitignore

--- server/routes/games.router.js
+++ server/routes/games.router.js
@@ -17,8 +17,8 @@ router.get('/:id', async (req, res) => {
     maxBodyLength: Infinity,
     url: 'https://api.igdb.com/v4/games',
     headers: { 
-      'Client-ID': '67cmtfjjiza9jdagz5nezdjsp7q2jb', 
-      'Authorization': 'Bearer w142s80hf0xtr3kmgrmebjz9zq425p', 
+      'Client-ID': process.env.CLIENT_ID, 
+      'Authorization': process.env.AUTHORIZATION, 
       'Accept': 'application/json', 
       'Content-Type': 'text/plain', 
       'Cookie': '__cf_bm=DlxOwur5Inz60Em5pSZ8qM6mIJ8zo8cGcLsP4MmkyzE-1687898318-0-AWpMyxT06Kj2MklbNv93eCb1egi0Vt85eA4EYmWNjeru8GTlGBDh6nypsqF4rylHca08iuupp6sgSEqyja6HiDA='
